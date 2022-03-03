2b829b474392aa7f8ce299501a49630d1469809a
Ldpw0xi1m1d <99180755+Ldpw0xi1m1d@users.noreply.github.com>
2022-03-03 23:02:19 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,14 +5,14 @@
 
 double pown(double value, uint16_t n) {
     double value1;
-   value1 = value;
-   if (n == 0) {
+value1 = value;
+ if (n == 0) {
      return 1;
      }
     for (int i = 1; i < n; i++) {
      value1 = value * value1;
     }
-   return value1;
+ return value1;
 }
 
 uint64_t fact(uint16_t n) {
@@ -38,32 +38,32 @@ double expn(double x, uint16_t count) {
       for (int i = 1; i <= count; i++) {
        exp = calcItem(x, i);
       exp1 = exp1 + exp;
-   }
+ }
     return exp1;
 }
 
 double sinn(double x, uint16_t count) {
-     double sinx, sinx1;
-      sinx1 = x;
-       if (count == 1) {
-        sinx1 = x;
-   }
-    for (int i = 1; i <= count; i++) {
-        sinx = pown(-1, i) * calcItem(x, 2 * i + 1);
-         sinx1 = sinx1 + sinx;
+  double sinx, sinx1;
+  sinx1 = x;
+  if (count == 1) {
+  sinx1 = x;
+}
+  for (int i = 1; i <= count; i++) {
+  sinx = pown(-1, i) * calcItem(x, 2 * i + 1);
+  sinx1 = sinx1 + sinx;
   }
   return sinx1;
 }
 
 double cosn(double x, uint16_t count) {
-   double cosx, cosx1;
+ double cosx, cosx1;
+  cosx1 = 1;
+   if (count == 1) {
     cosx1 = 1;
-     if (count == 1) {
-       cosx1 = 1;
      }
-   for (int i = 1; i < count; i++) {
-    cosx = pown(-1, i) * calcItem(x, 2 * i);
-      cosx1 = cosx1 + cosx;
-    }
-   return cosx1;
+for (int i = 1; i < count; i++) {
+  cosx = pown(-1, i) * calcItem(x, 2 * i);
+   cosx1 = cosx1 + cosx;
+  }
+ return cosx1;
 }
