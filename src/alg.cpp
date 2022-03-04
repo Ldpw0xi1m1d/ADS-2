0b78ca7250280458016b19056f8e8802f525e4bc
Ldpw0xi1m1d <99180755+Ldpw0xi1m1d@users.noreply.github.com>
2022-03-04 12:52:24 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -57,10 +57,10 @@ double cosn(double x, uint16_t count) {
 double cosx, cosx1;
 cosx1 = 1;
 if (count == 1) {
- cosx1 = 1;
+cosx1 = 1;
 }
 for (int i = 1; i < count; i++) {
 cosx = pown(-1, i) * calcItem(x, 2 * i);
- cosx1 = cosx1 + cosx;
+cosx1 = cosx1 + cosx;
 } return cosx1;
 }
